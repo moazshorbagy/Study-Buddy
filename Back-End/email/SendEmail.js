064a9cdef2e465de86be92ce77535bba777568d3
moazshorbagy <moazshorbagy@users.noreply.github.com>
2018-04-07 16:02:37 +0200

    BackEnd: added picture in email

--- Back-End/email/SendEmail.js
+++ Back-End/email/SendEmail.js
@@ -1,4 +1,11 @@
 var nodemailer = require('nodemailer');
+var cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+  cloud_name: 'moazshorbagy', 
+  api_key: '294895332157276', 
+  api_secret: 'Ix1bJtnVmunWq96z90qKI7zD-TU' 
+});
 
 function sendEmail(receiver){
 
@@ -15,7 +22,7 @@ var mailOptions = {
     to: receiver,
     subject: 'Study Buddy Team',
     html: 'Welcome to <b style="color:Tomato;">Study Buddy</b>!<br><br>  \
-    <img src="Study Buddy.png"><br> \
+    <img src="http://res.cloudinary.com/moazshorbagy/image/upload/c_scale,h_172/v1523104793/Logo1.png"><br> \
     Should you ever encounter problems with your account or forget your password, we will contact you at this address.<br><br>  \
 Enjoy!<br> \
 Study Buddy Team' 
