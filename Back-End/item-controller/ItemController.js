01ec8a3e52cd9779b0285133e24c9d771213815a
ismaeel123 <37120315+ismaeel123@users.noreply.github.com>
2018-04-24 22:33:58 +0200

    requests (not complete) and adjustment of cart (again)

--- Back-End/item-controller/ItemController.js
+++ Back-End/item-controller/ItemController.js
@@ -34,13 +34,17 @@ ITEM_ROUTER.prototype.handleRoutes = function(router, connection) {
           });
         }
       });
-
+    }
+  });
+  
+      router.post("/insertRequest", VerifyToken, function(req, res) {
+        for(var i in req.body){
       query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
       table = [
         "Request",
-        "book_id",
-        "donor_id",
-        "user_id",
+        "BookID",
+        "DonorID",
+        "UserID",
         req.body[i]["bookID"],
         req.body[i]["donorID"],
         req.userId
@@ -57,10 +61,49 @@ ITEM_ROUTER.prototype.handleRoutes = function(router, connection) {
         }
       });
     }
-    res.json({
-      Error: false,
-      Message: "Status successfully changed and request has been added"
+    });
+    
+    
+
+  router.get("/getRequest", VerifyToken, function(req, res) {
+    var query = "SELECT * FROM request WHERE DonorID = "+ req.userId;
+    connection.query(query, function(err, rows) {
+      if (err) {
+        console.log(err);
+        res.json({ Error: true, Message: "Error executing MySQL query", statusCode: "500" });
+      } else {
+        res.json({ Error: false, Requests: rows, n: rows.length, statusCode: "200" });
+      }
     });
   });
+
+  router.post("/getRequest_user", VerifyToken, function(req, res) {
+    var query = "SELECT user_name,book_title FROM user,book WHERE user_id = "+ req.body.UserID + " AND book_id= "+req.body.BookID
+     connection.query(query, function(err, rows) {
+      if (err) {
+        console.log(err);
+        res.json({ Error: true, Message: "Error executing MySQL query", statusCode: "500" });
+      } else if (rows == "")
+        res.json({ Error: true, Message: "No books found", statusCode: "404" });
+      else {
+        res.json({ Error: false, Book: rows, n: rows.length, statusCode: "200" });}
+    });
+  });
+
+  router.post("/getRequest_book", VerifyToken, function(req, res) {
+    var query = "SELECT * FROM book WHERE book_id = "+ req.body.BookID;
+     connection.query(query, function(err, rows) {
+      if (err) {
+        console.log(err);
+        res.json({ Error: true, Message: "Error executing MySQL query", statusCode: "500" });
+      } else if (rows == "")
+        res.json({ Error: true, Message: "No books found", statusCode: "404" });
+      else {
+        res.json({ Error: false, Book: rows, n: rows.length, statusCode: "200" });}
+    });
+  });
+
+
+
 };
 module.exports = ITEM_ROUTER;
